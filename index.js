bf7475dc5df16de320a4d65afe4519471af75b63
gias-uddin-swe <nirobh753@gmail.com>
2022-05-26 15:10:22 +0600

    fixed heroku apllication eroor with port changed

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.get("/app", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
